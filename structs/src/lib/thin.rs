69cf82609bc9a305c7ec625eb5272402dee525e5
Konstantinos Triantafyllou <ko.trian@gmail.com>
2019-09-23 16:42:51 +0200

    Add 2 impls of a generic Trait to Thin struct

--- structs/src/lib/thin.rs
+++ structs/src/lib/thin.rs
@@ -1,9 +1,22 @@
 use traits::lib::FooTrait;
+use traits::lib::GenericFooTrait;
 
 pub struct Thin;
 
 impl FooTrait for Thin {
     fn method(&self) -> u32 {
         0
+    }
+}
+
+impl GenericFooTrait<i32> for Thin {
+    fn method(&self) -> i32 {
+        42
+    }
+}
+
+impl GenericFooTrait<u32> for Thin {
+    fn method(&self) -> u32 {
+        42
     }
 }
