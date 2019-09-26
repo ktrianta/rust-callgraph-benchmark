9114d195f211496f60eccdf8c72b9b6526b47d44
Konstantinos Triantafyllou <ko.trian@gmail.com>
2019-09-26 23:58:30 +0200

    Improve the function pointers benchmark

--- function_pointers/src/lib.rs
+++ function_pointers/src/lib.rs
@@ -1,56 +1,109 @@
 pub mod lib {
     use structs::lib::fat::Fat;
     use traits::lib::FooTrait;
-    use traits::lib::BarTrait;
 
     pub type FatMethod = fn(&Fat) -> u32;
-    pub type FooMethod = fn(&FooTrait) -> u32;
-    pub type BarMethod = fn(&BarTrait) -> u32;
-    pub type GenMethod<T:FooTrait> = fn(T) -> u32;
-    
-    fn test(b: impl BarTrait) {
+    pub type FooMethod = fn(&dyn FooTrait) -> u32;
+    pub type GenMethod<T> = fn(&T) -> u32;
 
+    pub fn indirection(foo: &Fat, fun: FatMethod) -> u32 {
+        // function pointer call
+        // for<'r> fn(&'r structs::lib::fat::Fat) -> u32
+        // Call via function pointer 'fun'.
+        fun(foo)
+    }
+
+    pub fn indirection_generic<T: FooTrait>(foo: &T, fun: GenMethod<T>) -> u32 {
+        // function pointer call
+        // for<'r> fn(&'r T) -> u32
+        // Call via generic function pointer 'fun'.
+        fun(foo)
+    }
+
+    // Equivalent to indirection(foo: &Fat, fun: FatMethod) -> u32
+    // fn indirection_concretized_generic(foo: &Fat, fun: GenMethod<Fat>) -> u32 {
+    //     fun(foo)
+    // }
+
+    pub fn indirection_trait_object(foo: &dyn FooTrait, fun: FooMethod) -> u32 {
+        // function pointer call
+        // for<'r> fn(&'r (dyn traits::lib::FooTrait + 'r)) -> u32
+        // Call via function pointer 'fun', which accepts a trait object as argument.
+        fun(foo)
+    }
+
+    // Equivalent to indirection_trait_object(foo: &dyn FooTrait, fun: FooMethod) -> u32
+    // fn indirection_sixth(foo: impl FooTrait + 'static, fun: GenMethod<dyn FooTrait>) -> u32 {
+    //     fun(&foo)
+    // }
+
+    pub fn indirection_fn_trait(foo: &Fat, fun: &dyn Fn(&Fat) -> u32) -> u32 {
+        // instance method call (trait - std::ops::Fn::call)
+        // &dyn for<'r> std::ops::Fn(&'r structs::lib::fat::Fat) -> u32
+        // Call of Fn trait instance 'fun'.
+        fun(foo)
     }
 }
 
 pub mod bench {
+    use crate::lib::indirection;
+    use crate::lib::indirection_generic;
+    use crate::lib::indirection_trait_object;
+    use crate::lib::indirection_fn_trait;
     use structs::lib::fat::Fat;
-    use structs::lib::thin::Thin;
-    use traits::lib::FooTrait;
     use traits::lib::BarTrait;
+    use traits::lib::FooTrait;
 
-    use crate::lib::FatMethod;
-    use crate::lib::FooMethod;
-    use crate::lib::GenMethod;
+    mod helpers {
+        use super::FooTrait;
 
-    fn indirection_one(foo: &Fat, fun: &dyn Fn(&Fat) -> u32) {
-        println!("{}", fun(foo));
+        pub fn m1(obj: &dyn FooTrait) -> u32 {
+            obj.method()
+        }
+
+        pub fn m2(obj: &dyn FooTrait) -> u32 {
+            obj.method()
+        }
     }
-
-    fn indirection_two(foo: &Fat, fun: FatMethod) {
-        println!("{}", fun(foo));
-    }
-
-//    fn indirection_three(foo: impl FooTrait, fun: GenMethod<FooTrait>) {
-//        //println!("{}", fun(foo));
-//    }
-
-//    fn indirection_four(t: &Thin, fun: &dyn Fn(&FooTrait) -> String) {
-//        println!("{}", fun(t));
-//    }
 
     pub fn run() {
         let f = Fat(10);
 
-        indirection_one(&f, &Fat::method);
-        indirection_one(&f, &<Fat as FooTrait>::method);
+        // static function call
+        // function_pointers::lib::indirection
+        // Pointed function is part of Fat's implementation (struct impl).
+        indirection(&f, Fat::method);
 
-        indirection_two(&f, Fat::method);
-        indirection_two(&f, FooTrait::method);
-        indirection_two(&f, <Fat as BarTrait>::method);
+        // static function call
+        // function_pointers::lib::indirection
+        // Pointed function is part of FooTrait's implementation by Fat (trait impl).
+        indirection(&f, FooTrait::method);
 
-        //fun(&f);
-        //indirection_three(f, FooTrait::method);
-        //indirection_four(&Thin, &fun);
+        // static function call
+        // function_pointers::lib::indirection
+        // Pointed function is part of BarTrait's implementation by Fat (trait impl). The syntax
+        // used to specify the method is slightly different than in the last testcase but normally
+        // there should not be any significant difference. We include this case for completeness.
+        indirection(&f, <Fat as BarTrait>::method);
+
+        // static function call
+        // function_pointers::lib::indirection_generic
+        // Pointed function is generic.
+        indirection_generic(&f, Fat::method);
+        // The following two calls should be covered by the 'indirection' testcases.
+        // indirection_generic(&f, FooTrait::method);
+        // indirection_generic(&f, <Fat as BarTrait>::method);
+
+        // static function call
+        // function_pointers::lib::indirection_generic
+        // Pointed function accepts a trait object as an argument.
+        indirection_trait_object(&f, helpers::m2);
+
+        // static function call
+        // function_pointers::lib::indirection_fn_trait
+        indirection_fn_trait(&f, &Fat::method);
+        // The following two calls should be covered by the 'indirection' testcases.
+        // indirection_fn_trait(&f, &BarTrait::method);
+        // indirection_fn_trait(&f, &<Fat as FooTrait>::method);
     }
 }
