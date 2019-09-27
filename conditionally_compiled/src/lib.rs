55d2db02712e105ea713ba0757da9bf7b574e0bb
Konstantinos Triantafyllou <ko.trian@gmail.com>
2019-09-27 15:12:39 +0200

    Remove unrelated stuff from conditionally compiled benchmark

--- conditionally_compiled/src/lib.rs
+++ conditionally_compiled/src/lib.rs
@@ -1,82 +1,59 @@
 // TODO: Define how conditionally compiled code should be handled by call-graph generators.
 
 pub mod lib {
-    use traits::lib::FooTrait;
-    use traits::lib::BarTrait;
+    // Function 'bar' calls the version of 'foo' function that is compiled depending on whether
+    // feature 'foo' is defined or not during compilation.
+    pub fn bar() -> u32 {
+        // static function call
+        // conditionally_compiled::lib::foo
+        // Function 'foo' is conditionally compiled.
+        foo()
+    }
 
-    // There exist two versions of function 'foo' and which one is compiled depends on whether
-    // feature foo is defined during compilation. If it is, 'foo' returns the result of calling
-    // the 'method' method of trait object x. Otherwise, it returns the empty string. To achieve
-    // this 'foo' calls 'foo_private', which is also conditionally compiled.
     #[cfg(feature = "foo")]
-    pub fn foo(x: &dyn FooTrait) -> String {
-        foo_private(x)
+    pub fn foo() -> u32 {
+        // static function call
+        // conditionally_compiled::lib::base_one
+        base_one()
     }
 
     #[cfg(not(feature = "foo"))]
-    pub fn foo(x: &dyn FooTrait) -> String {
-        foo_private(x)
+    pub fn foo() -> u32 {
+        // static function call
+        // conditionally_compiled::lib::base_two
+        base_two()
     }
 
-    // 'foo_private' works as an indirection step. The 'foo' function that is compiled when
-    // the foo feature is enabled calls the 'foo_private' function compiled when the foo
-    // feature is enabled.
-    #[cfg(feature = "foo")]
-    fn foo_private(x: &dyn FooTrait) -> String {
-        x.method()
+    #[allow(dead_code)]
+    fn base_one() -> u32 {
+        1
     }
 
-    #[cfg(not(feature = "foo"))]
-    fn foo_private(_x: &dyn FooTrait) -> String {
-        String::new()
+    #[allow(dead_code)]
+    fn base_two() -> u32 {
+        2
     }
-
-    // There exist two versions of function 'bar' similar to those of function 'foo'.
-    #[cfg(feature = "bar")]
-    pub fn bar(x: &dyn BarTrait) -> String {
-        x.another_method()
-    }
-
-    #[cfg(not(feature = "bar"))]
-    pub fn bar(_x: &dyn BarTrait) -> String {
-        String::new()
-    }
-
 }
 
 pub mod bench {
     pub fn run() {
-        use structs::lib::fat::Fat;
-        use structs::lib::thin::Thin;
-        use traits::lib::FooTrait;
-        use traits::lib::BarTrait;
-        use crate::lib::foo;
         use crate::lib::bar;
+        use crate::lib::foo;
 
-        let fat = Fat(100);
-        let thin = Thin;
-        let foo_items: Vec<&dyn FooTrait> = vec![&fat, &thin];
-        let bar_items: Vec<&dyn BarTrait> = vec![&fat];  // Does not contain 'thin'. Compiler is aware
-                                                         // that BarTrait is not implemented for Thin.
+        // static function call
+        // conditionally_compiled::lib::bar
+        // Function 'bar' calls the conditionally compiled function 'foo'.
+        let num1 = bar();
 
-        for item in foo_items.into_iter() {
-            let result = foo(item);
+        // static function call
+        // conditionally_compiled::lib::foo
+        // Function 'foo' is conditionally compiled on feature 'foo'.
+        let num2 = foo();
 
-            // Function foo is conditionally compiled on feature 'foo'.
-            // If 'foo' is not defined the returned value is the empty String.
-            if !result.is_empty() {
-                println!("{}", result);
-            }
-        }
-
-        for item in bar_items.into_iter() {
-            let result = bar(item);
-
-            // Function bar is conditionally compiled on feature 'bar'.
-            // If 'bar' is not defined the returned value is the empty String.
-            if !result.is_empty() {
-                println!("{}", result);
-            }
-        }
+        // This is here to ensure that the above calls are not optimized away as dead code.
+        println!(
+            "Just making sure no code is deemed dead by the compiler: {}",
+            num1 + num2
+        );
     }
 }
